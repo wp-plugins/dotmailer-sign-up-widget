34ce399228ac2954d3357bbce2fed69683418dd8
dotMailer <dotMailer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-20 16:39:14 +0000

    3.1 Release with PHP closing tag fix
    git-svn-id: https://plugins.svn.wordpress.org/dotmailer-sign-up-widget/tags/3.1@807639 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/widget.js
@@ -0,0 +1,10 @@
+/*PG FIX*/
+/*jQuery.noConflict();*/  
+jQuery(document).ready(function(){
+
+    jQuery(".Date").datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
+});
+ 
